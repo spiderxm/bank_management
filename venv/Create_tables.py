ac78f675f7530b7e015aa7d55f7af072ddc97858
Mrigank  Anand <mrigank.rkee@rediffmail.com>
2020-05-22 15:13:24 +0530

    adding sql commands

--- venv/Create_tables.py
+++ venv/Create_tables.py
@@ -1,24 +1,90 @@
 import mysql.connector
+from datetime import datetime
+
 mydb = mysql.connector.connect(
-    host = "database.ct1ikgzgdh96.us-east-1.rds.amazonaws.com",
-    user = "admin",
-    passwd = "mrigank52"
+    host="database.ct1ikgzgdh96.us-east-1.rds.amazonaws.com",
+    user="admin",
+    passwd="mrigank52"
 )
 mycursor = mydb.cursor()
+try:
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS BANK ")
+except:
+    print("Error")
 
-# mycursor.execute("CREATE DATABASE BANK ")
+try:
+    mycursor.execute("USE BANK")
+except:
+    print("Error")
 
-mycursor.execute("USE BANK")
-
-mycursor.execute("create table if not exists account_holder( account_holder varchar(255), address varchar(255),  phone_number varchar(20), account_number varchar(20) primary key, account_type varchar(20)")
+try:
+    print("DROP TABLE IF EXISTS account_holder")
+except:
+    print("Error")
+try:
+    print("DROP TABLE IF EXISTS account_balance")
+except:
+    print("Error")
+try:
+    mycursor.execute("CREATE TABLE if NOT EXISTS account_holder("
+                     "account_holder varchar(255), "
+                     "email varchar(30), "
+                     "address varchar(255), "
+                     "phone_number varchar(20), "
+                     "account_number varchar(20) primary key, "
+                     "account_type varchar(20), "
+                     "amount decimal(30,5),"
+                     "account_creation_time datetime NOT NULL)")
+except:
+    print("Error")
+try:
+    print("CREATE TABLE if NOT EXISTS account_balance("
+          "account_number int primary key, "
+          "balance decimal(30,5), "
+          "foreign key(account_number) references account_holder(account_number))")
+except:
+    print("Error")
+try:
+    print("CREATE TABLE if NOT EXISTS balance_tranfer("
+          "from varchar(20), "
+          "to varchar(20), "
+          " amount decimal(30,5), "
+          "foreign key (from,to) references account_holder(account_number))")
+except:
+    print("Error")
+try:
+    print("CREATE TABLE if NOT EXISTS account_history("
+          "account_number varchar(20) NOT NULL , "
+          "payment_type varchar(10) NOT NULL, "
+          "balance_before decimal(30,5) NOT NULL, "
+          "balance_afterwards decimal(30,5)) NOT NULL, "
+          "transaction_time datetime NOT NULL)")
+except:
+    print("Error")
+try:
+    time1 = datetime.today()
+    time2 = datetime.today()
+    time3 = datetime.today()
+    time = time1, time2, time3
+    mycursor.execute("INSERT INTO account_holder VALUES "
+                     "('mrigank anand', 'mrigank@nith.ac.in', 'kailash boys hostel nith', '9999888877', '87151766626366737611', 'elite', 100000, %s), "
+                     "('aman chauhan', 'aman-d-1-n-only@nith.ac.in', 'kailash boys hostel nith', '9876543211', '92120805165080483546',' lite', 990000, %s ), "
+                     "('shekhar motiyan', 'shekhar@nith.ac.in', 'kailash boys hostel nith', '9999999999', '11458183925071153386', 'executive', 10000, %s),"
+                     , time)
+except:
+    print("Error")
+try:
+    print("INSERT INTO account_balance VALUES "
+          "('87151766626366737611', 100000),"
+          "('92120805165080483546', 990000),"
+          "('11458183925071153386', 10000)")
+except:
+    print("Error")
+try:
+    print("INSERT INTO account_history ")
+except:
+    print("Error")
 # try:
-#     mycursor.execute("SHOW TABLES")
+#     print("INSERT INTO ")
 # except:
-#     print("")
-#
-# for x in mycursor:
-#     print(x)
-
-mycursor.close()
-
-mydb.close()
+#     print("Error")
--- venv/bank.py
+++ venv/bank.py
@@ -1,31 +1,50 @@
 from random import randint
 import mysql.connector
+from datetime import datetime, date, time
 # mydb = mysql.connector.connect(
-#     host = "database.ct1ikgzgdh96.us-east-1.rds.amazonaws.com",
-#     user = "admin",
-#     passwd = "mrigank52",
-#     dbname = "BANK"
+#     host="database.ct1ikgzgdh96.us-east-1.rds.amazonaws.com",
+#     user="admin",
+#     passwd="mrigank52",
+#     dbname="BANK"
 # )
 # mycursor = mydb.cursor()
+
+
 def create_user():
     account_holder = input("Enter your name : ")
     address = input("Enter your address : ")
+    email = input("Enter your email : ")
     phone_number = input("Enter your phone number :")
     account_type = input("Choose account type between lite, elite, executive : ")
+    amount = input("Initial deposit in your account")
+    account_creation_time = datetime.today()
     account_number = str(randint(100 ** 9, (100 ** 10) - 1))
-    if len(account_holder) > 3 and len(address) > 9  and len(phone_number) > 7 and len(account_type) > 3:
+    if len(account_holder) > 3 and len(address) > 9 and len(phone_number) > 7 and len(account_type) > 3 and len(
+            email) > 6 and float(amount) > 0:
         print(account_holder, "your account number is ", account_number, "and keep it classified and safe")
         try:
-            mycursor.execute("INSERT INTO ")
-        except:
-            print("Error")
-    else:
-        print("Please fill the fields correctly")
-        create_user()
+            values = (account_holder, email, address, phone_number, account_number, account_type, float(amount), account_creation_time)
+            mycursor.execute("INSERT INTO account_holder VALUES (%s,%s,%s,%s,%s,%s, %s, %s)", values)
+            print("Your Account has been successfully created")
+            print("-----" * 2)
+        except :
+            print("There was some error in creating you account please try again later")
+            print("-----" * 2)
+        else:
+            print("Please fill the fields correctly")
+            create_user()
+
 
 # create_user()
 def withdrawal():
     account_number = input("Enter your account number : ")
+    mycursor.execute("SELECT account_number FROM account_holder")
+    account_numbers = mycursor.fetchall()
+    if account_number in account_numbers:
+        print(account_number)
+    else:
+        print("Account number invalid try again")
+        withdrawal()
     amount = input("Enter amount you want to withdraw : ")
     amount = float(amount)
     try:
@@ -43,17 +62,41 @@ def withdrawal():
     except:
         print("Error")
 
+
 def deposit():
     account_number = input("Enter your account number : ")
+    mycursor.execute("SELECT account_number FROM account_holder")
+    account_numbers = mycursor.fetchall()
+    if account_number in account_numbers:
+        print(account_number)
+    else:
+        print("Account number invalid try again")
+        deposit()
     amount = input("Enter amount you want to deposit : ")
     try:
-        mycursor.execute("UPDATE account_balance SET balance = balance + %s WHERE account_number = %s", amount, account_number)
+        mycursor.execute("UPDATE account_balance SET balance = balance + %s WHERE account_number = %s", amount,
+                         account_number)
     except:
         print("Error")
 
+
 def transfer():
     your_account_number = input("Enter your account number : ")
+    mycursor.execute("SELECT account_number FROM account_holder")
+    account_numbers = mycursor.fetchall()
+    if account_number in account_numbers:
+        print(account_number)
+    else:
+        print("Account number invalid try again")
+        transfer()
     account_number = input("Enter Account number of the account in which you want to transfer money : ")
+    mycursor.execute("SELECT account_number FROM account_holder")
+    account_numbers = mycursor.fetchall()
+    if account_number in account_numbers:
+        print(account_number)
+    else:
+        print("Account number invalid try again")
+        transfer()
     amount = input("Enter amount you want to withdraw : ")
     amount = float(amount)
     try:
@@ -64,10 +107,32 @@ def transfer():
     if mycursor.fetchone() >= amount:
         try:
             balance = balance - amount
-            mycursor.execute("UPDATE account_balance SET balance = %s WHERE account_number = %s", balance, your_account_number)
-            mycursor.execute("UPDATE account_balance SET balance = balance + %s WHERE account_number = %s", amount, account_number)
+            mycursor.execute("UPDATE account_balance SET balance = %s WHERE account_number = %s", balance,
+                             your_account_number)
+            mycursor.execute("UPDATE account_balance SET balance = balance + %s WHERE account_number = %s", amount,
+                             account_number)
             print("Transaction Successful")
         except:
             print("Error")
     else:
         print("Insufficient funds in your bank account")
+
+
+def passbook():
+    account_number = input("Enter your account number : ")
+    mycursor.execute("SELECT account_number FROM account_holder")
+    account_numbers = mycursor.fetchall()
+    if account_number in account_numbers:
+        print(account_number)
+        mycursor.execute("SELECT * FROM transactions WHERE account_number = %s", account_number)
+        print("----------")
+        for transaction in mycursor:
+            print(transaction)
+        print("----------")
+        print("Thanks for selecting our bank you are a valuable customer")
+    else:
+        print("Account number invalid try again")
+        passbook()
+
+
+print(datetime.today())
